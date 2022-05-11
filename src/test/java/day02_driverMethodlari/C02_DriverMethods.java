6be82d277ecadb78d6d3e10576b609aae7fa3b3b
Alikan <dralikan94@gmail.com>
2022-05-11 21:30:31 +0300

    selenium 3. gün

---
+++ src/test/java/day02_driverMethodlari/C02_DriverMethods.java
@@ -0,0 +1,17 @@
+package day02_driverMethodlari;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C02_DriverMethods {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.amazon.com");
+        System.out.println("title : "+driver.getTitle());
+        System.out.println("actual url : "+ driver.getCurrentUrl());
+    }
+}
---
+++ src/test/java/day03_locators/C01_Locators.java
@@ -0,0 +1,45 @@
+package day03_locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_Locators {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        // Amazona gidip nutella aratalım
+
+        driver.get("https://www.amazon.com");
+
+        /*
+        findElement(Locator) istediğimiz web elementini bize dondurur.
+        Biz de o web elementini kullanmak icin bir objeye assign ederiz.
+         */
+
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+
+        /*
+        Her hangi bir web elementine istediğimiz bir yazıyı yollamak istersek
+         */
+
+        aramaKutusu.sendKeys("Nutella" + Keys.ENTER);
+
+        //***olmadı***
+        //driver.get("https://www.google.com");
+        //WebElement aramaKutusuGoogle = driver.findElement(By.className("gLFyf gsfi"));
+        //aramaKutusuGoogle.sendKeys("Sümeyye Şeyma Kan");
+        //***olmadı***
+
+
+
+
+    }
+}
